0a10fcc0f9d94881a7cb0718eb72b292d072ee26
Alexander <alexanderkronmikheev@gmail.com>
2021-04-10 13:11:52 +0300

    initial commit

---
+++ homework-five/src/main/java/com/mikheev/homeworkfive/HomeworkFiveApplication.java
@@ -0,0 +1,13 @@
+package com.mikheev.homeworkfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkFiveApplication.class, args);
+    }
+
+}
